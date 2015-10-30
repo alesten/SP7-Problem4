3da8ad08220a3af354c5b49d7b696ed4149ec857
Alexander Steen <ale.steen@hotmail.com>
2015-10-30 01:12:07 +0100

    Added tests

--- app/app.js
+++ app/app.js
@@ -3,8 +3,55 @@
 var app = angular.module('myApp', []);
 
 
-app.filter('checkmark', function () {
-  return function (input) {
-    return input ? '\u2713' : '\u2718';
-  };
+app.filter('name', function () {
+    return function (person) {
+        return person.lastName + ", " + person.firstName;
+    };
 });
+
+app.directive('loginForm', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: 'loginForm.html'
+    };
+});
+
+app.factory('MyFactory', function () {
+    return{
+        titleCase: function (input) {
+            var words = input.split(' ');
+            var str = "";
+            for(var i = 0; i < words.length; i++){
+                var word = words[i];
+                
+                str += " " + word.substring(0,1).toUpperCase() + word.substring(1);
+            }
+            
+            return str.substring(1);
+        },
+        camelCase: function (input) {
+            var words = input.split(' ');
+            var str = "";
+            for(var i = 0; i < words.length; i++){
+                var word = words[i];
+                
+                str += word.substring(0,1).toUpperCase() + word.substring(1);
+            }
+            
+            return str;
+        },
+        dashCase: function (input) {
+            var find = ' ';
+            var re = new RegExp(find, 'g');
+
+            return input.replace(re, '-');
+        }
+    }
+});
+
+
+app.controller('FactoryController', ['$scope', 'MyFactory', function ($scope, MyFactory) {
+        $scope.titleCase = MyFactory.titleCase("my example factory");
+        $scope.camelCase = MyFactory.camelCase("my example factory");
+        $scope.dashCase = MyFactory.dashCase("my example factory");
+    }]);
--- test/tests.js
+++ test/tests.js
@@ -2,16 +2,50 @@
  * Remove the example below, before you start
  */
 
-describe('My Tests', function() {
+describe('My Tests', function () {
 
-  beforeEach(module('myApp'));
+    beforeEach(module('myApp'));
 
-  describe('checkmark', function() {
+    describe('name', function () {
 
-    it('should convert boolean values to unicode checkmark or cross',
-        inject(function(checkmarkFilter) {
-      expect(checkmarkFilter(true)).toBe('\u2713');
-      expect(checkmarkFilter(false)).toBe('\u2718');
-    }));
-  });
+        it('should make person object to lastName, firstName',
+                inject(function (nameFilter) {
+                    expect(nameFilter({firstName: 'Peter', lastName: 'Smith'})).toBe('Smith, Peter');
+                    expect(nameFilter({firstName: 'Alex', lastName: 'Steen'})).toBe('Steen, Alex');
+                }));
+    });
+
+    describe('MyFactory', function () {
+        var MyFactory;
+        beforeEach(inject(function (_MyFactory_) {
+            MyFactory = _MyFactory_;
+        }));
+        it('Should be my example service in diffrent styles', function () {
+            expect(MyFactory.titleCase("my example service")).toBe("My Example Service");
+            expect(MyFactory.camelCase("my example service")).toBe("MyExampleService");
+            expect(MyFactory.dashCase("my example service")).toBe("my-example-service");
+        });
+    });
+
+    describe('controller', function () {
+        var $controller;
+
+        beforeEach(inject(function (_$controller_) {
+            $controller = _$controller_;
+        }));
+
+        var $scope, controller;
+
+        beforeEach(function () {
+            $scope = {};
+            //This line doesn't work with the second controller
+            controller = $controller('FactoryController', {$scope: $scope});
+        });
+
+        it('test controller get text from factory', function () {
+            expect($scope.titleCase).toBe('My Example Factory');
+            expect($scope.camelCase).toBe('MyExampleFactory');
+            expect($scope.dashCase).toBe('my-example-factory');
+        });
+    });
 });
